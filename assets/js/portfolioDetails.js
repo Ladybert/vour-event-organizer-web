a15e0c280c7e12440028ac75979af79fe9783a18
FawzyCode <137638007+Ladybert@users.noreply.github.com>
2025-11-18 02:00:10 +0700

    save data js & fixing line 670 index.html attr "title"

---
+++ assets/js/portfolioDetails.js
@@ -0,0 +1,88 @@
+// portfolioData.js
+const portfolioDetails = [
+  {
+    id: "Details1-IwWk6CVcP6jshdI8",
+    title: "Design Poster Competition",
+    description: "",
+    images: [
+      "assets/img/detailsimg/project1/slide1.jpeg",
+      "assets/img/detailsimg/project1/slide2.jpeg",
+      "assets/img/detailsimg/project1/slide3.jpeg",
+      "assets/img/detailsimg/project1/slide4.jpeg",
+      "assets/img/detailsimg/project1/slide5.png",
+      "assets/img/detailsimg/project1/slide6.png",
+    ],
+    category: "", 
+    client: "", 
+    date: "", 
+    url: "" 
+  },
+  {
+    id: "Details2-ut93ccVlhhsjfWt5",
+    title: "Short Movie Competition",
+    description: "",
+    images: [
+      "assets/img/detailsimg/project2/slide1.png",
+      "assets/img/detailsimg/project2/slide2.png",
+      "assets/img/detailsimg/project2/slide3.png",
+      "assets/img/detailsimg/project2/slide4.png",
+      "assets/img/detailsimg/project2/slide5.png",
+      "assets/img/detailsimg/project2/slide6.png",
+      "assets/img/detailsimg/project2/slide7.png",
+    ],
+    category: "", 
+    client: "", 
+    date: "", 
+    url: "" 
+  },
+  {
+    id: "Details3-HOHi7dKoNrPANVW0",
+    title: "The Student Ultimate Chess Showdown",
+    description: "",
+    images: [
+      "assets/img/detailsimg/project3/slide1.jpg",
+      "assets/img/detailsimg/project3/slide2.jpg",
+      "assets/img/detailsimg/project3/slide3.jpg",
+      "assets/img/detailsimg/project3/slide4.jpg",
+      "assets/img/detailsimg/project3/slide5.jpg",
+      "assets/img/detailsimg/project3/slide6.jpg",
+      "assets/img/detailsimg/project3/slide7.jpg",
+    ],
+    category: "", 
+    client: "", 
+    date: "", 
+    url: "" 
+  },
+  {
+    id: "Details4-vGEA3pmN5KYbO9QV",
+    title: "Art's City'25",
+    description: "",
+    images: [
+      "assets/img/detailsimg/project4/slide1.jpg",
+      "assets/img/detailsimg/project4/slide2.jpg",
+      "assets/img/detailsimg/project4/slide3.jpg",
+      "assets/img/detailsimg/project4/slide4.jpg",
+      "assets/img/detailsimg/project4/slide5.jpg",
+      "assets/img/detailsimg/project4/slide6.jpg",
+      "assets/img/detailsimg/project4/slide7.jpg",
+    ],
+    category: "", 
+    client: "", 
+    date: "", 
+    url: "" 
+  },
+  {
+    id: "Details5-k8c8Cp3OY5axIFKU",
+    title: "Introduce Wedding Photography | Workshop",
+    description: "",
+    images: [
+      "assets/img/portfolio/project1-1.jpg",
+    ],
+    category: "", 
+    client: "", 
+    date: "", 
+    url: "" 
+  },
+];
+
+export default portfolioDetails;
